9b9d75709da01fd74f046fd97596815dc0d39908
Madboy0700 <bobimobi07@gmail.com>
2025-03-06 09:16:56 +0300

    Update play.py

--- ArchMusic/utils/inline/play.py
+++ ArchMusic/utils/inline/play.py
@@ -53,7 +53,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         [
             InlineKeyboardButton(
                 text=f"Duyuru", 
-                url=f"https://t.me/boxmusicduyuru"
+                url=f"https://t.me/ArthurDuyuru"
             )
         ],
         [
@@ -76,7 +76,7 @@ def telegram_markup_timer(_, chat_id, played, dur, videoid):
         [
             InlineKeyboardButton(
                 text=f"Duyuru", 
-                url=f"https://t.me/boxmusicduyuru"
+                url=f"https://t.me/ArthurDuyuru"
             )
         ],
         
@@ -106,7 +106,7 @@ def stream_markup(_, videoid, chat_id):
         [
             InlineKeyboardButton(
                 text=f"Duyuru", 
-                url=f"https://t.me/boxmusicduyuru"
+                url=f"https://t.me/ArthurDuyuru"
             )
         ],
         
@@ -147,7 +147,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
         [
             InlineKeyboardButton(
                 text=f"Duyuru", 
-                url=f"https://t.me/boxmusicduyuru"
+                url=f"https://t.me/ArthurDuyuru"
             )
         ],
         
@@ -170,7 +170,7 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=f"Duyuru", 
-                url=f"https://t.me/boxmusicduyuru"
+                url=f"https://t.me/ArthurDuyuru"
             )
         ],
         

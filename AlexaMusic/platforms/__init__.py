673aa0e33d806d111ec1c7f91cc63d856bdffc7e
Mehmet67676767 <kurt67143@gmail.com>
2025-05-22 15:10:49 +0300

    Update __init__.py

--- AlexaMusic/platforms/__init__.py
+++ AlexaMusic/platforms/__init__.py
@@ -1,12 +1,10 @@
-from .Apple import Apple
-from .Carbon import Carbon
-from .JioSavan import Saavn
-from .Resso import Resso
-from .Soundcloud import SoundCloud
-from .Spotify import Spotify
-from .Telegram import Telegram
-from .Youtube import YouTube
-
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import Youtube
 
 class PlaTForms:
     def __init__(self):

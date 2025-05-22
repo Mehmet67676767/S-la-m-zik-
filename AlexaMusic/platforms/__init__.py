5d55e0953ea2b34a4d919b29c8ffca19bc827630
Mehmet67676767 <kurt67143@gmail.com>
2025-05-22 15:08:32 +0300

    Update __init__.py

--- AlexaMusic/platforms/__init__.py
+++ AlexaMusic/platforms/__init__.py
@@ -1,19 +1,20 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
+from .Apple import Apple
+from .Carbon import Carbon
+from .JioSavan import Saavn
+from .Resso import Resso
+from .Soundcloud import SoundCloud
+from .Spotify import Spotify
+from .Telegram import Telegram
+from .Youtube import YouTube
 
 
-from .Apple import AppleAPI
-from .Carbon import CarbonAPI
-from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
-from .Spotify import SpotifyAPI
-from .Telegram import TeleAPI
-from .Youtube import YouTubeAPI
+class PlaTForms:
+    def __init__(self):
+        self.apple = Apple()
+        self.carbon = Carbon()
+        self.saavn = Saavn()
+        self.resso = Resso()
+        self.soundcloud = SoundCloud()
+        self.spotify = Spotify()
+        self.telegram = Telegram()
+        self.youtube = YouTube()

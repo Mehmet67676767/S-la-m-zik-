5c2ec460a1f330d11d90b0be9dd0f5558ce43e6d
Mehmet67676767 <kurt67143@gmail.com>
2025-06-10 18:08:08 +0300

    Update help.py

--- ArchMusic/utils/inline/help.py
+++ ArchMusic/utils/inline/help.py
@@ -12,7 +12,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from LostMuzik import app
+from ArchMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):

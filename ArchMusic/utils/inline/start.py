8e916579dbe403f83653e33f7e5fe84345ca9443
Mehmet67676767 <kurt67143@gmail.com>
2025-06-10 20:08:21 +0300

    Update start.py

--- ArchMusic/utils/inline/start.py
+++ ArchMusic/utils/inline/start.py
@@ -106,7 +106,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                 InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"S_B_1 : "https://github.com/Mehmet67676767/Musicbotaa"
+                    text=_["S_B_6"], url=f"https://t.me/the_team_kumsal"
                 ),
             ]
         )
